a88bbe84d46a67ff8d7605c3c7da59a7c1c7a16a
Mateus Mota <mateusmota.js@gmail.com>
2023-01-16 23:23:43 -0300

    chore: add jest to lint staged

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: { '.+\\.ts$': 'ts-jest' }
+}
