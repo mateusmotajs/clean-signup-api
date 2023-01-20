ee8e9b092fa1f0af262fe3dac44519bad03706ab
Mateus Mota <mateusmota.js@gmail.com>
2023-01-20 00:05:46 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   transform: { '.+\\.ts$': 'ts-jest' }
 }
